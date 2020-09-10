dc70e58bf24537e353834087451e983023d0d5b1
EC2 Default User <ec2-user@ip-172-31-80-140.ec2.internal>
2020-09-10 06:50:06 +0000

    5章

--- sample_app/app/controllers/todolists_controller.rb
+++ sample_app/app/controllers/todolists_controller.rb
@@ -12,12 +12,24 @@ class TodolistsController < ApplicationController
 
     list.save
 
-    redirect_to '/top'
+    redirect_to todolists_path(list.id)
+  end
+
+
+  def index
+  @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
   end
 
   private
+
+
   def list_params
     params.require(:list).permit(:title, :body)
   end
+
 
 end
